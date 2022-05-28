5a7660959db16cee9c0252325fe9fb5b7eceeb9f
enli0r <milanniko19@gmail.com>
2022-05-28 05:03:26 +0200

    added line clamp plugin for tailwind

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,6 +2,6 @@
 
 <div class="w-full rounded-xl bg-white mb-6 p-3">
     <h3 class="font-bold mb-3">{{ $title }}</h3>
-    <p class="mb-5">{{ $description }}</p>
+    <p class="mb-5 line-clamp-3">{{ $description }}{{ $description }}{{ $description }}{{ $description }}</p>
     <small class="text-gray-400 font-semibold">{{ $postedbefore }}</small>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,5 +28,10 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/line-clamp'),
+    ],
+    
+
 };
